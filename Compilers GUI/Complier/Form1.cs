9ffc63e65e564c83661dd59b0fd67c495fc4a676
Ahmed M. Salah <ahmed.mohamed973@eng-st.cu.edu.eg>
2019-05-11 04:03:32 +0200

    Added GUI

---
+++ Compilers GUI/Complier/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace Complier
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog ofd = new OpenFileDialog();
+        string undo = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            richTextBox2.Clear();
+            richTextBox3.Clear();
+            TextWriter txt = new StreamWriter(@"..\\..\\..\\input.txt");
+            txt.Write(richTextBox1.Text);
+            txt.Close();
+
+            // TODO: Change path acoordingly
+            System.Diagnostics.Process.Start("cmd.exe", "/C C: & cd C:\\Users\\Ahmed\\Desktop\\Complier & project & pause");
+
+            richTextBox2.Text = File.ReadAllText(@"..\\..\\..\\output.txt");
+            richTextBox3.Text = File.ReadAllText(@"..\\..\\..\\symbol.txt");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Text = File.ReadAllText(ofd.FileName);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text == "")
+                return;
+            undo = richTextBox1.Text;
+            richTextBox1.Text = "";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = undo;
+        }
+    }
+}
